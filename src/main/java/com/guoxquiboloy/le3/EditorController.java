3b048ca0ba19d1d1c8305e74d7dbe3bdab2518be
Raindeeru <derv.gonzales17@gmail.com>
2024-09-19 00:18:01 +0800

    andito na lahat ng elements ng basic implementation

--- src/main/java/com/guoxquiboloy/le3/EditorController.java
+++ src/main/java/com/guoxquiboloy/le3/EditorController.java
@@ -2,8 +2,15 @@ package com.guoxquiboloy.le3;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 
-public class Editor {
+public class EditorController {
     @FXML TextArea typeArea;
+    @FXML Button saveButton;
+
+    @FXML
+    private void Save() throws IOException{
+        System.out.println("Pinindot Mo Save");
+    }
 }
